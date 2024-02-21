cdde9342ebf9a0214bf12b6ee7f9eef2200df3a3
tonyykosseifyy <tonykosseify123@gmail.com>
2024-02-21 09:21:07 +0200

    add: include folder for imports

---
+++ include/headers.h
@@ -0,0 +1,17 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <libgen.h> 
+#include <pthread.h>
+#include <stdbool.h>
+
+
+
+#endif 
